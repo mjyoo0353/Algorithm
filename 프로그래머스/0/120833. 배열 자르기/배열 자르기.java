a133db6c864c7e6db3b669a0bf3dc9a5584a5756
Minji Yoo <74090803+mjyoo0353@users.noreply.github.com>
2024-07-16 00:47:37 -0700

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -2,8 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
+        int[] answer = new int[num2 - num1 + 1];
+        int k = 0;
         
+        for(int i = num1; i <= num2; i++){
+            answer[k++] = numbers[i];
+        }
         return answer;
     }
 }
