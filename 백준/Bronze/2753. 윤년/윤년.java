585145b5cae64a12d8a014543183a9a0a941d1ec
Minji Yoo <74090803+mjyoo0353@users.noreply.github.com>
2024-07-17 21:16:14 -0700

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -3,13 +3,9 @@ import java.util.*;
 class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int inputYear = sc.nextInt();
+        int year = sc.nextInt();
         
-        if(inputYear % 4 == 0 && inputYear % 100 != 0){
-            System.out.print("1");
-        }else if(inputYear % 100 != 0 && inputYear % 400 == 0){
-            System.out.print("1");
-        }else if(inputYear % 400 == 0){
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
             System.out.print("1");
         }else{
             System.out.print("0");
