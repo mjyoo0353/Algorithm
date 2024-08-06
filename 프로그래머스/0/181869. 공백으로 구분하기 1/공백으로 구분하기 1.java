9990eebfd450ef34f9dfe2fa4d3ddd04f50c31ef
Minji Yoo <74090803+mjyoo0353@users.noreply.github.com>
2024-08-06 12:30:57 -0700

    [level 0] Title: 공백으로 구분하기 1, Time: 0.19 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,7 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = my_string.split("\\s");
-        //파라미터로 공백을 의미하는 정규식 "\s"를 전달
+        String[] answer = my_string.split(" ");
         
         return answer;
     }
