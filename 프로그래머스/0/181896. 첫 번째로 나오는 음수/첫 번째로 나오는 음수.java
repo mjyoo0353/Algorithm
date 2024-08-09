f0de8967404cfaaade2e64304f2546dfb116defa
Minji Yoo <74090803+mjyoo0353@users.noreply.github.com>
2024-08-09 11:22:25 -0700

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
-        int count = 0;
         
         for(int i = 0; i < num_list.length; i++){
             if(num_list[i] < 0){
-                answer = i;
-                break;
+                return i;
             }
-            answer = -1;
         }
-        return answer;
+        return -1;
     }
 }
