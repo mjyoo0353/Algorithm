1aaccf94a2f471ceb92f055f6d65ef994e6c7b17
Minji Yoo <74090803+mjyoo0353@users.noreply.github.com>
2024-10-02 23:27:21 -0700

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 16072 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,18 +1,22 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int sum = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         
         for(int i = 1; i <= N; i++){
-            int A = in.nextInt();
-            int B = in.nextInt();
-            sum = A + B;
-            System.out.println("Case #" + i + ": " + sum);
+            st = new StringTokenizer(br.readLine(), " ");
+            bw.write("Case #" + i + ": " + (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
         }
- 
+        br.close();
+        bw.flush();
+        bw.close();
+        
     }
 }
