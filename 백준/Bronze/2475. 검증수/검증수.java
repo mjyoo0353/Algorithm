f6c0b824d65bbd45fe0409d5cd51305a54ef0737
Minji Yoo <74090803+mjyoo0353@users.noreply.github.com>
2024-10-04 17:23:50 -0700

    [Bronze V] Title: 검증수, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,15 +1,16 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] nums = new int[5];
         int sum = 0;
         
         for(int i = 0; i < nums.length; i++){
-            nums[i] = in.nextInt();
-        }
-        for(int i = 0; i < nums.length; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
             sum += nums[i] * nums[i];
         }
         System.out.println(sum % 10);
