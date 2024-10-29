cacb48195f66cc28579681b46aa643ceae8110c4
Minji Yoo <ryuminji0353@gmail.com>
2024-10-28 22:24:08 -0700

    [level 1] Title: 크기가 작은 부분문자열, Time: 9.83 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(String t, String p) {
-        long tLength = t.length();
+        int tLength = t.length();
         int pLength = p.length();
         int answer = 0;
-
-    
-        for(int i = 0; i < tLength - pLength + 1; i++){
-            long temp = Long.parseLong(t.substring(i, i + pLength));
+        
+        for(int i = 0; i <= tLength - pLength; i++){
+            long temp = Long.parseLong(t.substring(i, pLength + i));
             if(temp <= Long.parseLong(p)){
                 answer++;
             }
