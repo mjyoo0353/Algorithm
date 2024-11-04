1c1f6c2298a311c58b25247af1635248f82b4931
Minji Yoo <ryuminji0353@gmail.com>
2024-11-04 00:42:41 -0800

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] nums = new int[9];
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        
+        for(int i = 0; i < 9; i++){
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = 0; i < 9; i++){
+            if(nums[i] > max){
+                max = nums[i];
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
