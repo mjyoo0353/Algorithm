bd7e179c33680e686c679343450e47172dad3c70
Minji Yoo <ryuminji0353@gmail.com>
2024-11-06 16:43:15 -0800

    [level 1] Title: 완주하지 못한 선수, Time: 84.94 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        Map<String, Integer> map = new HashMap<>();
+        String answer = "";
+        
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        for(String c : completion){
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1){
+                answer = entry.getKey();
+            }
+        }
+        return answer;
+    }
+}
