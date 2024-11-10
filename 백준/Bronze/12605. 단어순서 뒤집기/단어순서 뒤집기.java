0f984631210f7a54010b3f29367ec2ede99e0b2f
Minji Yoo <ryuminji0353@gmail.com>
2024-11-09 16:12:35 -0800

    [Bronze II] Title: 단어순서 뒤집기, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -6,18 +6,16 @@ public class Main {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
+            
         for(int i = 0; i < n; i++){
             String str = br.readLine();
             String[] arr = str.split(" ");
             
             System.out.print("Case #" + (i+1) + ": ");
-            
-            for(int j = arr.length - 1; j >= 0; j--){
+            for(int j = arr.length - 1; j >= 0; j--) {
                 System.out.print(arr[j] + " ");
             }
         }
         System.out.println();
-        br.close();
     }
 }
