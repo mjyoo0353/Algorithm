6e20949fc7b282d6ae4bef510cba4e64fc851308
Minji Yoo <ryuminji0353@gmail.com>
2024-11-11 14:37:42 -0800

    [level 1] Title: 폰켓몬, Time: 3.86 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,22 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int max = nums.length / 2;
-        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        int n = nums.length / 2;
         
-        for(int i = 0; i < nums.length; i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i], 0);
-            }
-            map.put(nums[i], map.get(nums[i]) + 1);
+        for(int num : nums){
+            set.add(num);
         }
-        int typeNum = map.size();
-        if(typeNum < max || typeNum == max){
-            answer = typeNum;
-        } else {
-            answer = max;
-        }
-        return answer;
+        return set.size() > n ? n : set.size();
+        
     }
 }
