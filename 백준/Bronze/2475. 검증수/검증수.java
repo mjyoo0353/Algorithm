a8bc4680123f47cfc4c69f20d7dfde9dcafc507d
Minji Yoo <ryuminji0353@gmail.com>
2024-11-12 17:49:21 -0800

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17620 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,16 +1,14 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
         int sum = 0;
         
-        for(int i = 0; i < 5; i++){
-            int num = Integer.parseInt(st.nextToken());
-            sum += num * num;
+        for(String i : input.split(" ")){
+            int n = Integer.parseInt(i);
+            sum += n * n;
         }
         System.out.println(sum % 10);
     }
