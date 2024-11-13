53b38aba81d052fbb8ec59e562f3b13790847418
Minji Yoo <ryuminji0353@gmail.com>
2024-11-12 17:31:19 -0800

    [Bronze V] Title: 두 수 비교하기, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-        int A, B;
-        Scanner sc = new Scanner(System.in);
-        A = sc.nextInt();
-        B = sc.nextInt();
+    public static void main(String[] args) {
         
-        if(A > B){
-            System.out.print(">");
-        }else if(A < B){
-            System.out.print("<");
-        }else if(A == B){
-            System.out.print("==");
+        Scanner in = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(in.nextLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        if(a > b) {
+            System.out.println(">");
+        }else if(a < b) {
+            System.out.println("<");
+        }else {
+            System.out.println("==");
         }
     }
 }
