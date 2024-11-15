b851f785f9e83a646840ae43d2fad855f056b8bc
Minji Yoo <ryuminji0353@gmail.com>
2024-11-14 17:11:20 -0800

    [Bronze II] Title: 비슷한 전화번호 표시, Time: 372 ms, Memory: 41884 KB -BaekjoonHub

---
+++ 백준/Bronze/26041. 비슷한 전화번호 표시/비슷한 전화번호 표시.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine(), " ");
+        String b = sc.nextLine();
+        int count = 0;
+        
+        while(st.hasMoreTokens()) {
+            String a = st.nextToken();
+            if(a.equals(b)){
+                continue;
+            }else if(a.length() > b.length() && a.substring(0, b.length()).equals(b)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
