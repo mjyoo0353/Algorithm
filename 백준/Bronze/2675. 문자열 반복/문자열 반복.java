2bb095a04e4154cce9148422ba714a0abe2e995a
Minji Yoo <ryuminji0353@gmail.com>
2024-11-14 16:22:28 -0800

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         
         for(int i = 0; i < n; i++){
@@ -14,10 +15,11 @@ public class Main {
             
             for(int j = 0; j < input.length(); j++){
                 for(int k = 0; k < num; k++){
-                    System.out.print(input.charAt(j));
+                    sb.append(input.charAt(j));
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
