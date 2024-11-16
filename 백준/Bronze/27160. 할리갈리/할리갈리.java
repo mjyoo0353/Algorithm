f61f07a6d01205af5e4df603b7d7b114571f7df1
Minji Yoo <ryuminji0353@gmail.com>
2024-11-15 18:30:50 -0800

    [Bronze II] Title: 할리갈리, Time: 292 ms, Memory: 39152 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.java
+++ 백준/Bronze/27160. 할리갈리/할리갈리.java
@@ -1,29 +1,23 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         Map<String, Integer> map = new HashMap<>();
-        String answer = "";
-        
-        for(int i = 0; i < n; i++){
+
+        for(int i = 0 ; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             String key = st.nextToken();
-            int cnt = Integer.parseInt(st.nextToken());
-            map.put(key, map.getOrDefault(key, 0) + cnt);
+            int value = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0) + value);
         }
-        
         if(map.containsValue(5)){
-            answer = "YES";
-        } else {
-            answer=  "NO";
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
         }
-        bw.write(answer);
-        bw.flush();
-        bw.close();
     }
 }
