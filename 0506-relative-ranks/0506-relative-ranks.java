0126cfd6a56bd4b41f4db9dd77b370e2153f4afd
Minji Yoo <ryuminji0353@gmail.com>
2024-11-24 23:01:26 -0800

    Time: 9 ms (55.91%), Space: 45.4 MB (92.24%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -2,28 +2,30 @@ class Solution {
     public String[] findRelativeRanks(int[] score) {
        
        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
        for(int i = 0; i < score.length; i++){
         pq.add(score[i]);
        }
-       
-       String[] str = new String[pq.peek() + 1];
+
+        Map<Integer, String> map = new HashMap<>();
        for(int i = 1; i <= score.length; i++){
         if(i == 1){
-            str[pq.poll()] = "Gold Medal";
+            map.put(pq.poll(), "Gold Medal");
         }else if(i == 2){
-            str[pq.poll()] = "Silver Medal";
+            map.put(pq.poll(), "Silver Medal");
         }else if(i == 3){
-            str[pq.poll()] = "Bronze Medal";
+            map.put(pq.poll(), "Bronze Medal");
         }else {
-            str[pq.poll()] = String.valueOf(i);
+            map.put(pq.poll(), String.valueOf(i));
         }
        }
 
-       String[] answer = new String[score.length];
-       for(int i = 0; i < score.length; i++){
-        answer[i] = str[score[i]];
-       }
-       return answer;
+        String[] answer = new String[score.length];
+        for(int i = 0; i < score.length; i++){
+            answer[i] = map.get(score[i]);
+        }
+        return answer;
+       
        
     }
 }
