f19f813b9b07f4e33cfa218121e7c3b14f92b284
Minji Yoo <ryuminji0353@gmail.com>
2024-11-26 18:26:33 -0800

    [Silver IV] Title: 덱, Time: 292 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -3,14 +3,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
+        /*
+        덱을 구현하고, 입력으로 주어진 명령을 처리하는 문제
+        1. 첫째 줄에는 명령의 수 N을 입력 받음
+        2. 둘째 줄부터는 N개의 줄만큼 명령을 하나씩 입력받음
+        3. 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력함
+        */
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Deque<Integer> deque = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(sc.nextLine(), " ");
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String input = st.nextToken();
             if(input.equals("push_front")){
                 deque.addFirst(Integer.parseInt(st.nextToken()));
