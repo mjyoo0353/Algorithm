200ea22c89a18bfc01697b5f0d1330455ce5f891
Minji Yoo <ryuminji0353@gmail.com>
2024-12-01 20:59:42 -0800

    [Silver II] Title: 최소 힙, Time: 248 ms, Memory: 25624 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -7,16 +7,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < n; i++){
             int input = Integer.parseInt(br.readLine());
             if(input == 0 && pq.isEmpty()){
-                System.out.println(0);
+                sb.append("0\n");
             }else if(input == 0 && !pq.isEmpty()){
-                System.out.println(pq.poll());
+                sb.append(pq.poll()).append("\n");
             }else {
                 pq.add(input);
             }
         }
+        System.out.print(sb);
     }
 }
