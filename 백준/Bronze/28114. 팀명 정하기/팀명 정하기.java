b8e2dd4f3feacaeaaf358041c7fead73a1f68645
Minji Yoo <ryuminji0353@gmail.com>
2024-12-02 00:22:11 -0800

    [Bronze II] Title: 팀명 정하기, Time: 68 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Bronze/28114. 팀명 정하기/팀명 정하기.java
+++ 백준/Bronze/28114. 팀명 정하기/팀명 정하기.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] num = new int[3];
+        Integer[] num = new Integer[3];
         int[] year = new int[3];
         Map<Integer, String> map = new HashMap<>();
         
@@ -15,15 +15,13 @@ public class Main {
             String name = st.nextToken().substring(0, 1);
             map.put(num[i], name);
         }
-        Arrays.sort(num);
+        Arrays.sort(num, Collections.reverseOrder());
         Arrays.sort(year);
         
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
         for(int i = 0; i < 3; i++){
             sb1.append(year[i]);
-        }
-        for(int i = 2; i >= 0; i--){
             sb2.append(map.get(num[i]));
         }
         System.out.println(sb1 + "\n" + sb2);
