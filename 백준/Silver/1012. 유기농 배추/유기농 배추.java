5cc3ab8c63e2bc1668b35a5162e8ee289563f006
Minji Yoo <ryuminji0353@gmail.com>
2024-12-09 16:30:24 -0800

    [Silver II] Title: 유기농 배추, Time: 92 ms, Memory: 13344 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{    
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int M, N, K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken()); //배추밭의 가로길이
+            N = Integer.parseInt(st.nextToken()); //세로길이
+            K = Integer.parseInt(st.nextToken()); //배추가 있는 위치의 개수
+
+            map = new int[M][N];
+            visited = new boolean[M][N];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[x][y] = 1;
+            }
+
+            int count = 0;
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        dfs(i ,j);
+                        count++;
+                    }
+                }
+            }
+            //최소의 배추흰지렁이 마리 수를 출력
+            System.out.println(count);
+        }
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N && !visited[nx][ny] && map[nx][ny] == 1) {
+                dfs(nx, ny);
+            }
+        }
+    }
+}
+
+            
